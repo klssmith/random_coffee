ea6773599baa3056684f7907880189d6ef5c70d8
Katie Smith <katie.smith@digital.cabinet-office.gov.uk>
2016-05-16 17:13:52 +0100

    Create test for displaying the results of a draw

--- spec/features/displaying_draw_spec.rb
+++ spec/features/displaying_draw_spec.rb
@@ -7,4 +7,20 @@ feature "making the draw" do
       expect(page).to have_content "There are no draw results"
     end
   end
+
+  context "when a draw has been made" do
+    before(:each) do
+      FactoryGirl.create(:member)
+      FactoryGirl.create(:member, firstname: "Sarah", lastname: "Jones", email: "sbrown@example.com")
+    end
+
+    scenario "displays the names of the members" do
+      visit "/matches/new"
+      click_link "Make the draw"
+      visit "/matches"
+      expect(page).not_to have_content "There are no draw results"
+      expect(page).to have_content "Joe Bloggs"
+      expect(page).to have_content "Sarah Jones"
+    end
+  end
 end
