554a4378f92d2e612d730312d2a3a36c8e71238e
Katie Smith <katie.smith@digital.cabinet-office.gov.uk>
2016-05-17 13:01:40 +0100

    Add methods to matches controller for shuffling members

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -4,6 +4,20 @@ class MatchesController < ApplicationController
   end
 
   def create
+    shuffle_members
     redirect_to '/matches'
+  end
+
+private
+  def shuffle_members
+    shuffled_members = Member.all.shuffle
+    populate_matches(shuffled_members)
+  end
+
+  def populate_matches(shuffled_members)
+    Match.delete_all
+    shuffled_members.each_slice(2) do |slice|
+      Match.create(member1_id: slice[0].id, member2_id: slice[1].id)
+    end
   end
 end
