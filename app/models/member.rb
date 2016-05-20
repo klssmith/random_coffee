09120ecad84f5f98df3c8e8b465e4f02bdcff153
Katie Smith <katie.smith@digital.cabinet-office.gov.uk>
2016-05-20 16:08:07 +0100

    Add .match method to Member model
    This added the .match method which allows you to find a member's matches.
    It wasn't possible to get this method with a simple 'has_one' relationship as a Member can have a match in one of two columns in the Match table.

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,4 +1,20 @@
 class Member < ActiveRecord::Base
   validates :firstname, :lastname, :email, presence: true
   validates :email, uniqueness: true
+
+  def match
+    if self.match_one
+      Member.find(match_one.member2_id)
+    elsif self.match_two
+      Member.find(match_two.member1_id)
+    end
+  end
+
+  def match_one
+    Match.find_by(member1_id: self.id)
+  end
+
+  def match_two
+    Match.find_by(member2_id: self.id)
+  end
 end
