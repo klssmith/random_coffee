2c96868e5bde20952d7b47909c522365c9637306
Katie Smith <katie.smith@digital.cabinet-office.gov.uk>
2016-05-27 14:22:41 +0100

    Refactor some feature tests
    The following feature test files were refactored:
    - displaying_users
    - leaving
    - signing_up
    
    A sign_in helper file was also created.

--- spec/features/leaving_spec.rb
+++ spec/features/leaving_spec.rb
@@ -1,11 +1,8 @@
 require 'rails_helper'
 
 feature "leaving Random Coffee" do
-  before(:each) do
-    FactoryGirl.create(:member)
-  end
-
   scenario "removes a member when they click the link" do
+    FactoryGirl.create(:member)
     visit "/members"
     click_link "Joe: leave Random Coffee"
     expect(page).not_to have_content "Joe Bloggs"
