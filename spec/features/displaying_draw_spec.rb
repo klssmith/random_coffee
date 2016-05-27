ebaf5beb6620e01ed250aeafde665a968fb0d22e
Katie Smith <katie.smith@digital.cabinet-office.gov.uk>
2016-05-27 14:49:03 +0100

    Add feature tests for displaying the date of the latest draw
    Added Timecop gem to mock the current time for the tests.

--- spec/features/displaying_draw_spec.rb
+++ spec/features/displaying_draw_spec.rb
@@ -1,19 +1,29 @@
 require 'rails_helper'
 
 feature "making the draw" do
+  before do
+    FactoryGirl.create(:member)
+    FactoryGirl.create(:member, firstname: "Sarah", lastname: "Jones", email: "sbrown@example.com")
+    Timecop.freeze("2016-05-27 14:39:58")
+  end
+
+  after do
+    Timecop.return
+  end
+
   context "when a draw has never been made" do
     scenario "displays a message that there is no draw" do
       visit "/matches"
       expect(page).to have_content "There are no draw results"
     end
+
+    scenario "does not display a draw time" do
+      visit "/matches"
+      expect(page).not_to have_content "27 May 2016"
+    end
   end
 
   context "when a draw has been made" do
-    before(:each) do
-      FactoryGirl.create(:member)
-      FactoryGirl.create(:member, firstname: "Sarah", lastname: "Jones", email: "sbrown@example.com")
-    end
-
     scenario "displays the names of the members" do
       visit "/matches/new"
       click_link "Make the draw"
@@ -21,6 +31,13 @@ feature "making the draw" do
       expect(page).not_to have_content "There are no draw results"
       expect(page).to have_content "Joe Bloggs"
       expect(page).to have_content "Sarah Jones"
+    end
+
+    scenario "displays the time of the last draw" do
+      visit "/matches/new"
+      click_link "Make the draw"
+      visit "/matches"
+      expect(page).to have_content "27 May 2016"
     end
   end
 end
