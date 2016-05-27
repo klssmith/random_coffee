d35919f0efc7e9770e1c7ff67dad8e118b8fc9ea
Katie Smith <katie.smith@digital.cabinet-office.gov.uk>
2016-05-27 15:56:13 +0100

    Refactor Match controller
    Created a class method for the Match model to check the draw time (instead of doing this in the controller). Added tests for the new .draw_time method.

--- app/models/match.rb
+++ app/models/match.rb
@@ -1,4 +1,10 @@
 class Match < ActiveRecord::Base
   belongs_to :member1, class_name: "Member", foreign_key: :member1_id
   belongs_to :member2, class_name: "Member", foreign_key: :member2_id
+
+  def self.draw_time
+    if Match.any?
+      self.first.created_at.strftime("%d %B %Y")
+    end
+  end
 end
--- spec/models/match_spec.rb
+++ spec/models/match_spec.rb
@@ -3,4 +3,22 @@ require 'rails_helper'
 RSpec.describe Match, type: :model do
   it { is_expected.to belong_to(:member1).class_name("Member") }
   it { is_expected.to belong_to(:member2).class_name("Member") }
+
+  describe ".draw_time" do
+    before { Timecop.freeze("2016-05-27 14:39:58") }
+    after { Timecop.return }
+
+    context "when there are no matches" do
+      it "returns nil" do
+        expect(Match.draw_time).to be_nil
+      end
+    end
+
+    context "when there are matches" do
+      it "returns the date of the first match" do
+        FactoryGirl.create(:match)
+        expect(Match.draw_time).to eq "27 May 2016"
+      end
+    end
+  end
 end
