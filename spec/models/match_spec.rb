4e2caa1b5812dd30b90d6a0a7b698ec7492247b8
Katie Smith <katie.smith@digital.cabinet-office.gov.uk>
2016-05-27 15:42:56 +0100

    Add Shoulda tests for Match model

--- spec/models/match_spec.rb
+++ spec/models/match_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 
-# RSpec.describe Match, type: :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+RSpec.describe Match, type: :model do
+  it { is_expected.to belong_to(:member1).class_name("Member") }
+  it { is_expected.to belong_to(:member2).class_name("Member") }
+end
